43a9b64dad82f01fab9e9bb7fed4de2a4cb1c3f2
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-10-16 11:28:30 +0200

    Add files via upload

---
+++ Vozilo-lista-u-XML/Vozilo-Lista-Forms-main/WinFormsXML/WinFormsXML/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.IO;
+using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Cryptography;
+
+namespace WinFormsXML
+{
+    public partial class Form1 : Form
+    {
+        List<Vozilo> brm = new List<Vozilo>(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(txtBrojKotaca.Text) % 2 == 1)
+                {
+                    MessageBox.Show("Nije paran broj.", "Greška",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                Vozilo brum = new Vozilo(txtModel.Text, Convert.ToInt32(txtGodinaProizvodnje.Text), Convert.ToInt32(txtBrojKotaca.Text), ToString(Vozilo.vrsta));
+
+                txtModel.Clear();
+                txtGodinaProizvodnje.Clear();
+                txtBrojKotaca.Clear();
+
+                brm.Add(brum);
+
+
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message, "Pogresan unos",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+            txtIspis.Clear();
+
+            txtIspis.Text = "Model:" + "\nGod Proizvodnje" + "\nBroj kotaca" + "\nVozilo je:" + "\n";
+
+            foreach (Vozilo brum in brm)
+            {
+                txtIspis.AppendText("\n\n" + brum.ToString());
+            }
+        }
+
+
+        private void xmlbtn_Click(object sender, EventArgs e)
+        {
+            string vrsta;
+            if (Convert.ToInt32(txtBrojKotaca.Text) == 2)
+            {
+                vrsta = "Motor";
+            }
+            if (Convert.ToInt32(txtBrojKotaca.Text) == 4)
+            {
+                vrsta = "Automobil";
+            }
+            if (Convert.ToInt32(txtBrojKotaca.Text) > 4)
+            {
+                vrsta = "Kamion";
+            } 
+
+            var dokumentXmlVozilo = new XDocument();
+            var rootElem = new XElement("Vozila");
+            dokumentXmlVozilo.Add(rootElem);
+            foreach (Vozilo vozilo in brm)
+            {
+                
+                var voziloElem = new XElement("Vozilo",
+                    new XAttribute("Model", vozilo.model),
+                    new XAttribute("Godina-proizvodnje", vozilo.god),
+                    new XAttribute("Broj-kotaca", vozilo.brkt),
+                    new XElement("Vrsta", vozilo.Vrsta));
+                rootElem.Add(voziloElem);
+
+            }
+            foreach (Vozilo vozilo in brm)
+            {
+                xmlispis.AppendText(dokumentXmlVozilo.ToString());
+            }
+
+            XmlSerializer serialiser = new XmlSerializer(typeof(List<Vozilo>));
+
+            TextWriter filestream = new StreamWriter(@"C:\output.xml");
+
+            serialiser.Serialize(filestream, xmlispis);
+
+            filestream.Close();
+
+
+        }
+    }
+    }
+
+
---
+++ Vozilo-lista-u-XML/Vozilo-Lista-Forms-main/WinFormsXML/WinFormsXML/Vozilo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsXML
+{
+    public class Vozilo
+    {
+        public string model;
+        public int god, brkt;
+        public string vrsta;
+
+        public Vozilo(string model, int god, int brkt, string vrsta)
+        {
+            this.Model = model;
+            this.God = god;
+            this.Brkt = brkt;
+            this.Vrsta = vrsta;
+        }
+
+        #region getteri i setteri
+        public string Model { get => model; set => model = value; }
+        public int God { get => god; set => god = value; }
+        public int Brkt { get => brkt; set => brkt = (char)value; }
+        public string Vrsta { get => vrsta; set => vrsta = value; }
+
+        #endregion
+
+
+        public override string ToString()
+        {
+            string ispis = null;
+            string vrstav;
+
+            if (brkt == 2)
+            {
+                this.vrsta = "Motor";
+                ispis = this.Model + "\n" + this.God + "\n" + this.Brkt + "\n" + this.vrsta;
+            }
+            if (brkt == 4)
+            {
+                this.vrsta = "Automobil";
+                ispis = this.Model + "\n" + this.God + "\n" + this.Brkt + "\n" + this.vrsta;
+            }
+            if(brkt > 4)
+            {
+                this.vrsta = "Kamion";
+                ispis = this.Model + "\n" + this.God + "\n" + this.Brkt + "\n" + this.vrsta;
+            }
+           return ispis;
+        }
+    }
+}
